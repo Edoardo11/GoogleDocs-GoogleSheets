a9ae72d1f06dc479b184016b2df75af806bd0afc
bombvvv <bombardieri.manuele.studente@itispaleocapa.it>
2022-03-22 09:12:36 +0100

    ye

---
+++ script.js
@@ -0,0 +1,9 @@
+  function changeDcsStyle(){
+    var element = document.getElementById("DocsDiv");
+    element.style.display = "none";
+}
+
+function changeShtsStyle(){
+    var element = document.getElementById("SheetsDiv");
+    element.style.display = "none";
+}
