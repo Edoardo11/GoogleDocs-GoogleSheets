af5bd1d2c27063f1970fafdff5f4dbc15b1c24fb
Edoardo11 <edoardomirandola2001@gmail.com>
2022-03-23 14:41:46 +0100

    devo finire la request

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-  function changeDcsStyle(){
-    var element = document.getElementById("DocsDiv");
-    element.style.display = "none";
-    var element2 = document.getElementById("NewDocsDiv");
-    element2.style.display = "block";
+function changeDcsStyle(){
+  var element = document.getElementById("DocsDiv");
+  element.style.display = "none";
+  var element2 = document.getElementById("NewDocsDiv");
+  element2.style.display = "block";
+}
+
+function getDocs(){
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    //foreach
+    document.getElementById("demo").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "https://www.googleapis.com/drive/v3/files?pageSize=1000&q=mimeType%3D%27application%2Fvnd.google-apps.document%27%20and%20trashed%3Dfalse");
+  xhttp.send();
+}
+
+function createDocCard(result){
+  var container = document.querySelector("#copertine");
+  container.innerHTML = "";
+  result.forEach((element, i) => {
+      let cover = document.createElement("img");
+      cover.src="https://image.tmdb.org/t/p/w500" + element.poster_path;
+      cover.className = "copertina";
+      cover.setAttribute("onclick", "mostraDescrizione(" + i + ")");
+      cover.setAttribute("data-bs-toggle", "modal");
+      cover.setAttribute("data-bs-target", "#exampleModal");
+      container.appendChild(cover);
+  });
 }
 
 function changeShtsStyle(){
@@ -11,3 +35,23 @@ function changeShtsStyle(){
     var element2 = document.getElementById("NewSheetsDiv");
     element2.style.display = "block";
 }
+
+/*
+var risultatiRicerca;
+
+function cercaDoc(){
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === this.DONE) {
+                var result=JSON.parse(this.responseText);
+                    console.log(result.results);
+                    risultatiRicerca = result.results;
+                    creaLocandina(result.results);
+                    //addToCarusel(result.results);
+            }
+    });
+    xhr.open("GET", "https://www.googleapis.com/drive/v3/files");
+
+    xhr.send();
+}
+*/
